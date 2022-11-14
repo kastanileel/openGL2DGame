5827aa8de26094ce458918f6303abe57afe31419
Daniel Stempfle <stempfledaniel@gmx.de>
2022-11-14 10:19:30 +0100

    Add shooting

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -53,7 +53,7 @@ public:
     glm::mat4 translation;
     float speed;
     float radius;
-    bool isActive;
+    bool isActive = true;
     virtual void update(PlayerDirection* userInput, bool* spaceBarPress) = 0;
     virtual void draw() = 0;  
     virtual bool initializeVAOs() = 0;
@@ -77,7 +77,7 @@ public:
                     float mindistance = radius + cur->radius;
                     if (dist < mindistance) {
                         collidedObjects.push_back(cur);
-                        //std::cout << "Collision detected!" << std::endl;
+                        std::cout << "Collision detected!" << std::endl;
                     }
 
 
@@ -122,6 +122,7 @@ public:
 
             data = loadSpriteBasedOnState();
         }
+
         this->draw();
     }
     void draw() override{
@@ -335,13 +336,15 @@ public:
             0,0,0,1
         );
         speed = 0.01f;
-        isActive = false;
+        isActive = true;
         radius = 0.1f;
         loadAllSpritesIntoData();
         data = loadSpriteBasedOnState();
     }
    
     void update(PlayerDirection* userInput, bool* spaceBarPress) override {
+
+        
         if (x > translation[0][3])
         {
             translation[0][3] += 0.001f;
@@ -489,8 +492,189 @@ public:
 
 };
 
+class Bullet: public GameObject {
+
+    int hitpoints;
+    glm::vec2 position;
+    PlayerDirection direction;
+    std::chrono::steady_clock::time_point shootTime;
+    float lifeTime = 1000;
+
+public:
+    Bullet(float x, float y, PlayerDirection dir, std::chrono::steady_clock::time_point shoot) {
+        direction = dir;
+        float ver = xRes / yRes;
+        float size = 0.2f;
+       
+        position.x = x - 0.14f;
+        position.y = y - 0.2f;
+        shootTime = shoot;
+        loadAllSpritesIntoData();
+        data = loadSpriteBasedOnState();
+    }
+    void update(PlayerDirection* userInput, bool* spaceBarPress) override {
+        if ((std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - shootTime).count()) > lifeTime) {
+            isActive = false;
+        }
+        switch (direction)
+        {
+        case front:
+            position.y -= 0.05f;
+            break;
+        case back:
+            position.y += 0.05f;
+            break;
+        case left:
+            position.x -= 0.05f;
+            break;
+        case right:
+            position.x += 0.05f;
+            break;
+        case none:
+            position.y -= 0.05f;
+            break;
+        default:
+            break;
+        }
+
+        float size = 0.08f;
+        float res = xRes / yRes;
+
+        translation = glm::mat4(
+            size, 0, 0, position.x,
+            0, size * res, 0, position.y,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        );
+        this->draw();
+    }
+    void draw() override {
+
+
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        //glTexSubImage2D(GL_TEXTURE0, 0, 0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, data );
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+
+        GLfloat matrix = glGetUniformLocation(programID, "mv");
+        glUniformMatrix4fv(matrix, 1, GL_FALSE, &translation[0][0]);
+
+        // Clear the screen
+
+
+        // Use our shader
+        glUseProgram(programID);
+
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+            3,  // size
+            GL_FLOAT,           // type
+            GL_FALSE,           // normalized?
+            0,                  // stride
+            (void*)0            // array buffer offset
+        );
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glUniform1i(textureSamplerID, 0);
+        glEnableVertexAttribArray(2);
+        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+        glVertexAttribPointer(
+            2,
+            2,
+            GL_FLOAT,
+            GL_FALSE,
+            0,
+            (void*)0
+        );
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, vertexbuffer_size); // 3 indices starting at 0 -> 1 triangle
+
+
+    }
+    bool initializeVAOs() override {
+        glGenVertexArrays(1, &VertexArrayID);
+        glBindVertexArray(VertexArrayID);
+
+        vertexbuffer_size = 6;
+
+        glm::vec2 triangleVertice1 = glm::vec2(0.0f, 0.0f);
+        glm::vec2 triangleVertice2 = glm::vec2(0.0f, 1.0f);
+        glm::vec2 triangleVertice3 = glm::vec2(1.0f, 1.0f);
+
+        glm::vec2 secTriangleVertice1 = glm::vec2(0.0f, 0.0f);
+        glm::vec2 secTriangleVertice2 = glm::vec2(1.0f, 1.0f);
+        glm::vec2 secTriangleVertice3 = glm::vec2(1.0f, 0.0f);
+
+        GLfloat g_vertex_buffer_data[18];
+        g_vertex_buffer_data[0] = triangleVertice1[0];
+        g_vertex_buffer_data[1] = triangleVertice1[1];
+        g_vertex_buffer_data[2] = 0.0f;
+        g_vertex_buffer_data[3] = triangleVertice2[0];
+        g_vertex_buffer_data[4] = triangleVertice2[1];
+        g_vertex_buffer_data[5] = 0.0f;
+        g_vertex_buffer_data[6] = triangleVertice3[0];
+        g_vertex_buffer_data[7] = triangleVertice3[1];
+        g_vertex_buffer_data[8] = 0.0f;
+        g_vertex_buffer_data[9] = secTriangleVertice1[0];
+        g_vertex_buffer_data[10] = secTriangleVertice1[1];
+        g_vertex_buffer_data[11] = 0.0f;
+        g_vertex_buffer_data[12] = secTriangleVertice2[0];
+        g_vertex_buffer_data[13] = secTriangleVertice2[1];
+        g_vertex_buffer_data[14] = 0.0f;
+        g_vertex_buffer_data[15] = secTriangleVertice3[0];
+        g_vertex_buffer_data[16] = secTriangleVertice3[1];
+        g_vertex_buffer_data[17] = 0.0f;
+
+        glGenBuffers(1, &vertexbuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+
+
+
+        textureSamplerID = glGetUniformLocation(programID, "myTextureSampler");
+
+        static const GLfloat g_uv_buffer_date[] = {
+            0.0f, 0.0f,
+            0.0f, 1.0f,
+            1.0f, 1.0f,
+            0.0f, 0.0f,
+            1.0f, 1.0f,
+            1.0f, 0.0f
+        };
+
+        glGenBuffers(1, &uvbuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_date), g_uv_buffer_date, GL_STATIC_DRAW);
+
+        return true;
+    }
+    bool cleanupVAOs() override {
+        // Cleanup VBO
+        glDeleteBuffers(1, &vertexbuffer);
+        glDeleteVertexArrays(1, &VertexArrayID);
+        return true;
+    }
+    unsigned char* loadSpriteBasedOnState() override {
+        return sprites[0];
+    }
+    void loadAllSpritesIntoData() override {
+        sprites.push_back(stbi_load("../sprites/bullet.png", &width, &height, &nrChannels, 4));
+
+    }
+};
+
 // public variables
 std::chrono::steady_clock::time_point lastUpdate;
+std::chrono::steady_clock::time_point lastShoot;
 
 std::vector<std::shared_ptr<GameObject>> gameObjects;
 PlayerDirection userInput = none;
@@ -531,19 +715,7 @@ int main( void )
             lastUpdate = std::chrono::steady_clock::now();
             updateAnimationLoop();
 
-            if (test) {
-               
-                std::shared_ptr<Enemy> e = std::make_shared<Enemy>(10, -1, -1);
-
-                e.get()->initializeVAOs();
-
-                gameObjects.push_back(e);
-
-                
-                
-               
-                test = false;
-            }
+            
         }
 	} // Check if the ESC key was pressed or the window was closed
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
@@ -564,7 +736,19 @@ int main( void )
 
 void updateAnimationLoop()
 {
-    std::cout << "___________ " << "\n";
+    if (test) {
+
+        std::shared_ptr<Enemy> e = std::make_shared<Enemy>(10, -1, -1);
+
+        e.get()->initializeVAOs();
+
+        gameObjects.push_back(e);
+
+
+
+
+        test = false;
+    }
     bool shooting = false;
     userInput = none;
     if (glfwGetKey(window, GLFW_KEY_SPACE)) {
@@ -589,11 +773,32 @@ void updateAnimationLoop()
     }
 
     glClear(GL_COLOR_BUFFER_BIT);
-
+    std::vector<std::shared_ptr<GameObject>> newObjs;
     for (int i = 0; i < gameObjects.size(); i++) {
         gameObjects[i].get()->update(&userInput, &shooting);
+        
+        if (gameObjects[i].get()->isActive) {
+            newObjs.push_back(gameObjects[i]);
+        }
     }
+
+    gameObjects.clear();
+    gameObjects = newObjs;
+
+    
      
+    if (shooting)
+    {
+        if ((std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - lastShoot).count()) > 500) {
+            lastShoot = std::chrono::steady_clock::now();
+            std::cout << x << ","<< y << "\n";
+
+
+            std::shared_ptr<Bullet> b = std::make_shared<Bullet>(Bullet(x, y, userInput, lastShoot));
+            b.get()->initializeVAOs();
+            gameObjects.push_back(b);
+        }
+    }
 
     glDisableVertexAttribArray(0);
   // Swap buffers
